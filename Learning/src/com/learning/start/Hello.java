9e295be74105776aae4741d51e09b808b8634f7a
Abhishek Mangudkar <Q2435@Corp.qualitykiosk.com>
2017-06-21 13:01:25 +0700

    New Final

--- Learning/src/com/learning/start/Hello.java
+++ Learning/src/com/learning/start/Hello.java
@@ -1,11 +1,12 @@
 package com.learning.start;
 
+
 public class Hello {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Starting New Github Push");
-
+		System.out.println("New Testing ");
 	}
 
 }
