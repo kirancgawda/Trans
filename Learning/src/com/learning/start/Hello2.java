5dbb3c744421b0d47d22c813452cde5946018aa0
Abhishek Mangudkar <Q2435@Corp.qualitykiosk.com>
2017-07-10 15:08:58 +0700

    New

---
+++ Learning/src/com/learning/start/Hello2.java
@@ -0,0 +1,13 @@
+package com.learning.start;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("Pushing New Build");
+		String value = "def";
+		
+		System.out.println(value);
+
+	}
+
+}
