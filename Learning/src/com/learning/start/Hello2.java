de3eee224513a400b561c7246bf855148fdc29cd
Abhishek Mangudkar <Q2435@Corp.qualitykiosk.com>
2017-07-10 16:30:38 +0700

    Update

--- Learning/src/com/learning/start/Hello2.java
+++ Learning/src/com/learning/start/Hello2.java
@@ -4,6 +4,7 @@ public class Hello2 {
 
 	public static void main(String[] args) {
 		System.out.println("Pushing New Build");
+		System.out.println("Pushing Latest Build");
 		String value = "def";
 		
 		System.out.println(value);
